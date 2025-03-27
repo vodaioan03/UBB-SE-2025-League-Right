2af92b16a3a59332577634d221420299dd94125c
bogdangosa <bogdan.gosa@gmail.com>
2025-03-27 13:41:05 +0200

    added navigation from team messi and tab to admin page

---
+++ Duo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+
+
+using Duo.ViewModels.Base;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Duo.ViewModels
+{
+    public class MainPageViewModel: ViewModelBase
+    {
+        public MainPageViewModel()
+        {
+
+        }
+
+        public void HandleNavigationSelectionChanged(NavigationViewSelectionChangedEventArgs args)
+        {
+
+        }
+
+    }
+
+}
--- Duo/Views/Pages/AdminMainPage.xaml.cs
+++ Duo/Views/Pages/AdminMainPage.xaml.cs
@@ -12,7 +12,6 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using Duo.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -22,29 +21,11 @@ namespace Duo.Views.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class AdminMainPage : Page
     {
-        private MainPageViewModel _viewModel;
-
-        public MainPage()
+        public AdminMainPage()
         {
             this.InitializeComponent();
-            _viewModel = new MainPageViewModel();
-        }
-        private void openUserViewButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void openAdminViewButton_Click(object sender, RoutedEventArgs e)
-        {
-            contentFrame.Navigate(typeof(AdminMainPage));
-        }
-
-        private void NavigationView_SelectionChanged(object sender, NavigationViewSelectionChangedEventArgs args)
-        {
-            _viewModel.HandleNavigationSelectionChanged(args);
         }
     }
-
 }
