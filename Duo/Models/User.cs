08b5669f5e7ea9f5db588b44e883f1f6ca98ba6e
denis <denisgre205@gmail.com>
2025-03-28 16:42:24 -0700

    Added methods for UserService, not implemented

---
+++ Duo/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duo.Models;
+
+public class User
+{
+    public int UserId { get; set; }
+    public string Username { get; set; }
+
+    public User(int userId, string username)
+    {
+        UserId = userId;
+        Username = username;
+    }
+
+    public User(string username)
+    {
+        Username = username;
+    }
+}
--- Duo/Services/UserService.cs
+++ Duo/Services/UserService.cs
@@ -1,6 +1,42 @@
-namespace DefaultNamespace;
+using Duo.Models;
+using System;
+
+namespace Duo.Services;
 
 public class UserService
 {
-    
+    public User GetById(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public User GetByUsername(string username)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CreateUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetLastCompletedSection(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetLastCompletedQuiz(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateUserSectionProgress(int userId, int newLastCompletedId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateUserQuizProgress(int userId, int newLastQuizCompleted)
+    {
+        throw new NotImplementedException();
+    }
 }
