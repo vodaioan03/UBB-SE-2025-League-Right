b319271ab839342ef7e25d3fb377ef51d724bfa6
denisdenis05 <66214092+denisdenis05@users.noreply.github.com>
2025-03-29 09:26:19 -0700

    Revert "Add CreateQuizModal component for quiz creation"

--- Duo/Views/Pages/AdminMainPage.xaml.cs
+++ Duo/Views/Pages/AdminMainPage.xaml.cs
@@ -12,9 +12,6 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using Duo.Views.Components.Modals;
-using Duo.Models.Exercises;
-using Duo.Models.Quizzes;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -29,34 +26,6 @@ namespace Duo.Views.Pages
         public AdminMainPage()
         {
             this.InitializeComponent();
-
-            // Subscribe to modal events
-            CreateQuizModal.QuizCreated += CreateQuizModal_QuizCreated;
-            CreateQuizModal.ModalClosed += CreateQuizModal_ModalClosed;
-        }
-
-        private void CreateQuiz_Click(object sender, RoutedEventArgs e)
-        {
-            QuizModalOverlay.Visibility = Visibility.Visible;
-        }
-
-        private async void CreateQuizModal_QuizCreated(object sender, QuizCreatedEventArgs e)
-        {
-            // TODO: Handle the new quiz creation
-            // You can add your logic here to save the quiz to your data store
-            var dialog = new ContentDialog
-            {
-                Title = "Success",
-                Content = $"Quiz '{e.Title}' created successfully with {e.Quiz.GetExerciseCount()} exercises!",
-                CloseButtonText = "OK",
-                XamlRoot = this.XamlRoot
-            };
-            await dialog.ShowAsync();
-        }
-
-        private void CreateQuizModal_ModalClosed(object sender, EventArgs e)
-        {
-            QuizModalOverlay.Visibility = Visibility.Collapsed;
         }
 
         public void OpenCreateExamPage_Click(object sender, RoutedEventArgs e)
