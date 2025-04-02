ec0d0d33b51af72264307fc91e9543be8a0e0315
denis <denisgre205@gmail.com>
2025-04-02 10:13:21 -0700

    Added difficulty

--- Duo/Views/Pages/QuizPage.xaml.cs
+++ Duo/Views/Pages/QuizPage.xaml.cs
@@ -22,6 +22,7 @@ using Duo.Models.Exercises;
 using static Duo.Views.Components.AssociationExercise;
 using static Duo.Views.Components.MultipleChoiceExercise;
 using static Duo.Views.Components.FillInTheBlanksExercise;
+using Duo.Models;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -72,6 +73,7 @@ namespace Duo.Views.Pages
             }
         }
 
+
         private void LoadCurrentExercise()
         {
             if (ViewModel != null && ViewModel.Exercises != null)
@@ -80,6 +82,74 @@ namespace Duo.Views.Pages
 
                 if (currentExercise != null)
                 {
+                    // Remove any existing difficulty indicators
+                    var mainStackPanel = ExerciseContentControl.Parent as StackPanel;
+                    if (mainStackPanel != null)
+                    {
+                        var existingIndicators = mainStackPanel.Children.OfType<Grid>()
+                            .Where(g => g.Children.OfType<FontIcon>().Any() && g.Children.OfType<TextBlock>().Any())
+                            .ToList();
+                        foreach (var indicator in existingIndicators)
+                        {
+                            mainStackPanel.Children.Remove(indicator);
+                        }
+                    }
+
+                    // Add difficulty indicator
+                    var difficultyGrid = new Grid
+                    {
+                        Background = new SolidColorBrush(Microsoft.UI.Colors.Transparent),
+                        HorizontalAlignment = HorizontalAlignment.Right,
+                        Margin = new Thickness(0, 0, 0, 16),
+                        Padding = new Thickness(12, 6, 12, 6)
+                    };
+
+                    difficultyGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+                    difficultyGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+
+                    var icon = new FontIcon
+                    {
+                        FontSize = 16,
+                        Margin = new Thickness(0, 0, 8, 0)
+                    };
+
+                    var text = new TextBlock
+                    {
+                        FontSize = 14,
+                        Style = (Style)Application.Current.Resources["BodyStrongTextBlockStyle"]
+                    };
+
+                    switch (currentExercise.Difficulty)
+                    {
+                        case Difficulty.Easy:
+                            icon.Glyph = "\uE73E"; // Checkmark
+                            text.Text = "Easy";
+                            text.Foreground = new SolidColorBrush(Microsoft.UI.Colors.Green);
+                            break;
+                        case Difficulty.Normal:
+                            icon.Glyph = "\uE7C3"; // Warning
+                            text.Text = "Normal";
+                            text.Foreground = new SolidColorBrush(Microsoft.UI.Colors.Orange);
+                            break;
+                        case Difficulty.Hard:
+                            icon.Glyph = "\uE783"; // Star
+                            text.Text = "Hard";
+                            text.Foreground = new SolidColorBrush(Microsoft.UI.Colors.Red);
+                            break;
+                    }
+
+                    Grid.SetColumn(icon, 0);
+                    Grid.SetColumn(text, 1);
+
+                    difficultyGrid.Children.Add(icon);
+                    difficultyGrid.Children.Add(text);
+
+                    // Add the difficulty indicator to the main StackPanel
+                    if (mainStackPanel != null)
+                    {
+                        mainStackPanel.Children.Insert(1, difficultyGrid); // Insert after the back button
+                    }
+
                     if (currentExercise is Models.Exercises.AssociationExercise associationExercise)
                     {
                         var associationControl = new Components.AssociationExercise()
@@ -126,12 +196,10 @@ namespace Duo.Views.Pages
                     };
 
                     ExerciseContentControl.Content = endScreen;
-
                 }
-
             }
-
         }
+
 
         private async void ShowMessage(FrameworkElement parentElement, bool valid)
         {
