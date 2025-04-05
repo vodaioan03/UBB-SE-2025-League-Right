f8808908a53643e48859740519538bf67ecafc66
Sofariu Adrian-Radu <66952373+AdrianSofariu@users.noreply.github.com>
2025-04-05 16:21:51 +0300

    added vm folder

---
+++ DuoTesting/ViewModels/ViewModelBaseUT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuoTesting.ViewModels
+{
+    // / This is a test class for ViewModelBase
+    [TestClass]
+    internal class ViewModelBaseUT
+    {
+        [TestMethod]
+        public void TestSetProperty()
+        {
+            var viewModel = new 
+        }
+    }
+}
