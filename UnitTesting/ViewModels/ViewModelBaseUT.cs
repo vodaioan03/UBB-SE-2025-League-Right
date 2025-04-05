4028c0be27689ca765e4b9ee3b9cb9ff4c7c88dc
Sofariu Adrian-Radu <66952373+AdrianSofariu@users.noreply.github.com>
2025-04-05 16:11:30 +0300

    Changed xUnit test project to MSTest

---
+++ UnitTesting/ViewModels/ViewModelBaseUT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitTesting.ViewModels
+{
+    [TestClass]
+    internal class ViewModelBaseUT
+    {
+        //testing the ViewModelBase class
+        [TestMethod]
+        public void TestSetProperty()
+        {
+            var viewModel = new Mock
+    }
+}
