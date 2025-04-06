28226793ba47ad965e58d056760d2e6d417cca60
Andreea410 <andreeaungur494@yahoo.com>
2025-04-06 21:53:04 +0300

    Feat: Implemented the setup for the tests for RoadmapRepository

---
+++ DuoTesting/Repositories/RoadmapRepositoryUT.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Duo.Repositories;
+using Duo.Models.Roadmap;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+using DuoTesting.Helper;
+using System.Data.Common;
+
+namespace DuoTesting.Repositories
+{
+    [TestClass]
+    public class RoadmapRepositoryUT : TestBase
+    {
+        private IRoadmapRepository _repository = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _repository = new RoadmapRepository(DbConnection);
+        }
+
+    }
+}
