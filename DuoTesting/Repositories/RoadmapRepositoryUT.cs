dcb90ddaf621025d180cccdba792fcbc4e3180fe
Andreea410 <andreeaungur494@yahoo.com>
2025-04-06 22:06:51 +0300

    Feat: Implemented all tests for RoadmapRepository

--- DuoTesting/Repositories/RoadmapRepositoryUT.cs
+++ DuoTesting/Repositories/RoadmapRepositoryUT.cs
@@ -32,6 +32,59 @@ namespace DuoTesting.Repositories
             await _repository.DeleteAsync(newId);
         }
 
+        [TestMethod]
+        public async Task GetAllAsync_ShouldReturnList()
+        {
+            var all = await _repository.GetAllAsync();
+            Assert.IsNotNull(all);
+        }
 
+        [TestMethod]
+        public async Task AddAndGetByName_ShouldWork()
+        {
+            var name = $"TempRoadmap_{Guid.NewGuid()}";
+            var id = await _repository.AddAsync(new Roadmap { Name = name });
+
+            var byName = await _repository.GetByNameAsync(name);
+            Assert.IsNotNull(byName);
+            Assert.AreEqual(name, byName.Name);
+
+            await _repository.DeleteAsync(id);
+        }
+
+        [TestMethod]
+        public async Task AddAsync_InvalidName_ShouldThrow()
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentException>(() =>
+                _repository.AddAsync(new Roadmap { Name = " " }));
+        }
+
+        [TestMethod]
+        public async Task GetByIdAsync_InvalidId_ShouldThrow()
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentException>(() => _repository.GetByIdAsync(0));
+        }
+
+        [TestMethod]
+        public async Task GetByNameAsync_Invalid_ShouldThrow()
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentException>(() => _repository.GetByNameAsync(""));
+        }
+
+        [TestMethod]
+        public async Task DeleteAsync_InvalidId_ShouldThrow()
+        {
+            await Assert.ThrowsExceptionAsync<ArgumentException>(() => _repository.DeleteAsync(0));
+        }
+
+        [TestMethod]
+        public async Task DeleteAsync_ShouldRemoveRoadmap()
+        {
+            var id = await _repository.AddAsync(new Roadmap { Name = "ToBeDeleted" });
+
+            await _repository.DeleteAsync(id);
+
+            await Assert.ThrowsExceptionAsync<KeyNotFoundException>(() => _repository.GetByIdAsync(id));
+        }
     }
 }
