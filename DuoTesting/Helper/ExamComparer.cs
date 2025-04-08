fdc8dcf083f76b8e09205ad30e3119a440b9eefb
Andreea410 <andreeaungur494@yahoo.com>
2025-04-08 23:48:16 +0300

    Feat: Added a new class which will help me compare two Exam objects

---
+++ DuoTesting/Helper/ExamComparer.cs
@@ -0,0 +1,22 @@
+﻿using Duo.Models.Quizzes;
+using System.Collections.Generic;
+
+namespace DuoTesting.Helpers
+{
+    public class ExamComparer : IEqualityComparer<Exam>
+    {
+        public bool Equals(Exam? x, Exam? y)
+        {
+            if (x is null && y is null) return true;
+            if (x is null || y is null) return false;
+
+            return x.Id == y.Id &&
+                   x.SectionId == y.SectionId;
+        }
+
+        public int GetHashCode(Exam obj)
+        {
+            return HashCode.Combine(obj.Id, obj.SectionId);
+        }
+    }
+}
