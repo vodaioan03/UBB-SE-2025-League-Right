5dc5e2ca69d48c73255f8f2eb8166a0dcda40750
Andreea410 <andreeaungur494@yahoo.com>
2025-04-09 00:04:28 +0300

    Refactor: Changed the QuizComparer to implement BaseQuizComparer

--- DuoTesting/Helper/QuizComparer.cs
+++ DuoTesting/Helper/QuizComparer.cs
@@ -3,21 +3,8 @@ using System.Collections.Generic;
 
 namespace DuoTesting.Helpers
 {
-    public class QuizComparer : IEqualityComparer<Quiz>
+    public class QuizComparer : BaseQuizComparer<Quiz>
     {
-        public bool Equals(Quiz? x, Quiz? y)
-        {
-            if (x is null && y is null) return true;
-            if (x is null || y is null) return false;
 
-            return x.Id == y.Id &&
-                   x.SectionId == y.SectionId &&
-                   x.OrderNumber == y.OrderNumber;
-        }
-
-        public int GetHashCode(Quiz obj)
-        {
-            return HashCode.Combine(obj.Id, obj.SectionId, obj.OrderNumber);
-        }
     }
 }
