b82352e3813994a98f9a48b36eacdd49ab8576b6
Andreea410 <andreeaungur494@yahoo.com>
2025-04-08 23:49:21 +0300

    Feat: Added a class which will help compare two Quiz objects

---
+++ DuoTesting/Helper/QuizComparer.cs
@@ -0,0 +1,23 @@
+﻿using Duo.Models.Quizzes;
+using System.Collections.Generic;
+
+namespace DuoTesting.Helpers
+{
+    public class QuizComparer : IEqualityComparer<Quiz>
+    {
+        public bool Equals(Quiz? x, Quiz? y)
+        {
+            if (x is null && y is null) return true;
+            if (x is null || y is null) return false;
+
+            return x.Id == y.Id &&
+                   x.SectionId == y.SectionId &&
+                   x.OrderNumber == y.OrderNumber;
+        }
+
+        public int GetHashCode(Quiz obj)
+        {
+            return HashCode.Combine(obj.Id, obj.SectionId, obj.OrderNumber);
+        }
+    }
+}
