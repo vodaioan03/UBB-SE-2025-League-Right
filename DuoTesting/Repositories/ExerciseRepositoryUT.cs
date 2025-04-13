0f9a4a3711d5a9a22a1d63a68cd7c98ca06f90c8
Andreea410 <andreeaungur494@yahoo.com>
2025-04-13 17:04:23 +0300

    Refactor: Got to 100% test coverage for InMemoryExerciseRepository

--- DuoTesting/Repositories/ExerciseRepositoryUT.cs
+++ DuoTesting/Repositories/ExerciseRepositoryUT.cs
@@ -107,5 +107,65 @@ namespace DuoTesting.Repositories
         {
             await Assert.ThrowsExceptionAsync<ArgumentException>(() => _repository.DeleteExerciseAsync(0));
         }
+
+        [TestMethod]
+        public async Task GetByIdAsync_NonexistentId_ShouldThrow()
+        {
+            await Assert.ThrowsExceptionAsync<KeyNotFoundException>(() =>
+                _repository.GetByIdAsync(999));
+        }
+
+        [TestMethod]
+        public async Task ClearAll_ShouldResetRepository()
+        {
+            var exercise = new FlashcardExercise(0, "Q?", "A", Difficulty.Easy);
+            int id = await _repository.AddExerciseAsync(exercise);
+
+            ((InMemoryExerciseRepository)_repository).ClearAll();
+
+            await Assert.ThrowsExceptionAsync<KeyNotFoundException>(() =>
+                _repository.GetByIdAsync(id));
+        }
+
+        private class UnknownExercise : Exercise
+        {
+            public UnknownExercise() : base(0, "?", Difficulty.Easy) { }
+        }
+
+        [TestMethod]
+        public void AddExerciseAsync_UnsupportedType_ShouldThrow()
+        {
+            var repo = new InMemoryExerciseRepository();
+            Assert.ThrowsExceptionAsync<NotSupportedException>(() =>
+                repo.AddExerciseAsync(new UnknownExercise()));
+        }
+
+        [TestMethod]
+        public async Task GetAllExercisesAsync_ShouldReturnAllExercises()
+        {
+            var ex1 = new FlashcardExercise(0, "Q1", "A1", Difficulty.Easy);
+            var ex2 = new FlashcardExercise(0, "Q2", "A2", Difficulty.Hard);
+
+            int id1 = await _repository.AddExerciseAsync(ex1);
+            int id2 = await _repository.AddExerciseAsync(ex2);
+
+            var all = await _repository.GetAllExercisesAsync();
+
+            Assert.AreEqual(2, all.Count);
+        }
+
+        [TestMethod]
+        public async Task GetExamAndQuizExercises_ShouldReturnAll()
+        {
+            var ex = new FillInTheBlankExercise(0, "Largest ocean?", Difficulty.Easy, new List<string> { "Pacific" });
+            await _repository.AddExerciseAsync(ex);
+
+            var quizExercises = await _repository.GetQuizExercisesAsync(1);
+            var examExercises = await _repository.GetExamExercisesAsync(1);
+
+            Assert.AreEqual(1, quizExercises.Count);
+            Assert.AreEqual(1, examExercises.Count);
+        }
+
     }
 }
